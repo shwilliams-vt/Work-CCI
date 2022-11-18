7f645fba6f7ea8738f376d7d78da49b34bf27f8c
= <=>
2022-11-18 18:41:54 -0500

    Need to find microphone stream api for node

---
+++ Audio/getmicstream.js
@@ -0,0 +1,46 @@
+const getUserMedia = require('get-user-media-promise');
+const MicrophoneStream = require('microphone-stream').default;
+
+function startMicrophone() {
+
+  // Note: in most browsers, this constructor must be called in response to a click/tap, 
+  // or else the AudioContext will remain suspended and will not provide any audio data.
+  const micStream = new MicrophoneStream();
+
+  getUserMedia({ video: false, audio: true })
+    .then(function(stream) {
+      micStream.setStream(stream);
+    }).catch(function(error) {
+      console.log(error);
+    });
+
+  // get Buffers (Essentially a Uint8Array DataView of the same Float32 values)
+  micStream.on('data', function(chunk) {
+    // Optionally convert the Buffer back into a Float32Array
+    // (This actually just creates a new DataView - the underlying audio data is not copied or modified.)
+    const raw = MicrophoneStream.toRaw(chunk)
+    //...
+
+    // note: if you set options.objectMode=true, the `data` event will output AudioBuffers instead of Buffers
+   });
+
+  // or pipe it to another stream
+  micStream.pipe(/*...*/);
+
+  // Access the internal audioInput for connecting to another nodes
+  micStream.audioInput.connect(/*...*/);
+
+  // It also emits a format event with various details (frequency, channels, etc)
+  micStream.on('format', function(format) {
+    console.log(format);
+  });
+
+  
+ }
+
+ // Stop when ready
+function stopMicrophone() {
+    micStream.stop();
+  };
+
+  startMicrophone();
---
+++ Audio/speechtotext.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const speechConfig = sdk.SpeechConfig.fromSubscription("YourSpeechKey", "YourSpeechRegion");
+
+function fromStream() {
+    let pushStream = sdk.AudioInputStream.createPushStream();
+
+    fs.createReadStream("YourAudioFile.wav").on('data', function(arrayBuffer) {
+        pushStream.write(arrayBuffer.slice());
+    }).on('end', function() {
+        pushStream.close();
+    });
+ 
+    let audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+    let recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+    recognizer.recognizeOnceAsync(result => {
+        console.log(`RECOGNIZED: Text=${result.text}`);
+        recognizer.close();
+    });
+}
+fromStream();
